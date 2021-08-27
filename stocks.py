3ec501afdbc9356781f520fb0e2cd256407697e7
Mwanja Joel <joelsilverworks@gmail.com>
2021-08-27 23:53:09 +0300

    unleashed the streamlit app

---
+++ stocks.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# lets begin streamlit
+st.write("""
+
+# Simple stock price app
+
+Shown are the stock closing price and volume of Google!
+
+"""
+)
